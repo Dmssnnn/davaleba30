d1b179f4927126ba75b1c252c4baedf1f9b42935
Dmssnnn <129981482+Dmssnnn@users.noreply.github.com>
2023-06-14 11:38:33 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+let text = "dimitri, giorgi , vache ";
+let result = text.slice(8,15);
+let length = text.length;
+let replace = text.replace("dimitri", "aja-aja")
+const fruits = ["apple", "mango","blueberry"];
+fruits.push("apple2");
+fruits.shift();
+fruits.unshift("dimitra");
+fruits[0] = "zd";
+const bijebi = ["jora","delpiero","alika"]
+const gogoebi = ["gloria","gloria"];
+
+const arrayOfNames = ["gio","jimi", "delpiero"];
+const getSlice = arrayOfNames.slice(0,2);
+console.log(getSlice)
+const bijebiDaGogoebi = gogoebi.concat(bijebi);
+console.log(bijebiDaGogoebi)
+console.log(fruits);
+console.log(replace)
