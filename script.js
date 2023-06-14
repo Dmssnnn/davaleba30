90e48ffaf7cce67fe4aa7695019d6d30af58ae72
Dmssnnn <129981482+Dmssnnn@users.noreply.github.com>
2023-06-14 13:53:20 +0400

    update

--- script.js
+++ script.js
@@ -17,3 +17,48 @@ const bijebiDaGogoebi = gogoebi.concat(bijebi);
 console.log(bijebiDaGogoebi)
 console.log(fruits);
 console.log(replace)
+
+
+
+// 3..დაამატეთ ობიექტების მასივი (სახელი, გვარი, ასაკი, პროფესია, ქულა); 
+// map მეთოდის გამოყენებით შექმენით ახალი მასივი სადაც მხოლოდ სახელი და გვარი  იქნება. ['ნოკა ყიფიანი', 'გიორგი აბუთიძე'....]
+
+
+const massiveOfObjects = [
+    {
+        name : 'dimitri',
+        surname : 'alavidze',
+        age : 19,
+        profession : "web developer",
+        grade : 80,
+    },
+    {
+         name : 'givia',
+        surname : 'alavidze',
+        age : 89,
+        profession : "back-end developer",
+        grade :2,
+    }    
+]
+const onlyNamesMassive = [];
+// for ( i = 0 ; i < massiveOfObjects.length; i++){
+//     onlyNamesMassive.push(massiveOfObjects[i].name);
+//     const map
+// }
+const names = massiveOfObjects.map (massiveOfObjects => {
+    onlyNamesMassive.push(massiveOfObjects.name)
+    return onlyNamesMassive;
+})
+console.log(onlyNamesMassive)
+const onlyGradesMassive = [];
+const grades = massiveOfObjects.map (massiveOfObjects => {
+    onlyGradesMassive.push(massiveOfObjects.grade)
+    return onlyGradesMassive;
+})
+console.log (onlyGradesMassive);
+
+const sum =onlyGradesMassive.reduce((accumulator, currentValue ) => {
+    return accumulator * currentValue;
+})
+console.log(sum)
+
